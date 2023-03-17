a2336bcadc6736f439dec6384434cdd0063b07bc
onoriel <onoriel@gmail.com>
2023-03-17 13:36:14 +0100

    Update Alert.java

--- src/main/java/com/atsistemas/model/Alert.java
+++ src/main/java/com/atsistemas/model/Alert.java
@@ -1,27 +1,4 @@
 package com.atsistemas.model;
 
-public class Alert {
-
-	private String name;
-
-	public Alert() {
-	}
-
-	public Alert(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Alert[name='%s']", name);
-	}
-
+public record Alert (String name){
 }
